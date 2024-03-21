e1adf673377a56bd619f82b92e071fcdf86a9a20
Jun Ma (C) <jun.ma@wiredcraft.com>
2024-03-21 14:30:38 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -77,6 +77,7 @@ async function analyzeCode(
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
+- Do not provide other content with the JSON format response, the only response is the JSON.
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
